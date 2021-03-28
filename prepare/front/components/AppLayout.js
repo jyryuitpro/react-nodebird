b7bee8170c91ed684b85c36c4f449462522c30f7
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-03-29 06:01:02 +0900

    로그인 폼 만들기

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import {Menu, Input, Row, Col} from 'antd';
 
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+
 const AppLayout = ({children}) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <div>
             <Menu mode="horizontal">
@@ -22,7 +26,7 @@ const AppLayout = ({children}) => {
             </Menu>
             <Row gutter={8}>
                 <Col xs={24} md={6}>
-                    왼쪽메뉴
+                    {isLoggedIn ? <UserProfile /> : <LoginForm />}
                 </Col>
                 <Col xs={24} md={12}>
                     {children}
