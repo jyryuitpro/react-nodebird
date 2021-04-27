9cedf8ee9d2260ac030c740225fb6819ca4157d9
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-27 22:51:49 +0900

    더미 데이터로 로그인하기

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -7,7 +7,11 @@ const ButtonWrapper = styled.div`
   margin-top: 10px;
 `;
 
-const LoginForm = () => {
+const FormWrapper = styled(Form)`
+  padding: 10px;
+`;
+
+const LoginForm = ({setIsLoggedIn}) => {
     const [id, setId] = useState('');
     const [password, setPassword] = useState('');
 
@@ -21,8 +25,13 @@ const LoginForm = () => {
 
     // const style = useMemo(() => ({marginTop: 10}), []);
 
+    const onSubmitForm = useCallback(() => {
+        console.log(id, password);
+        setIsLoggedIn(true);
+    }, [id, password]);
+
     return (
-        <Form>
+        <FormWrapper onFinish={onSubmitForm}>
             <div>
                 <label htmlFor="user-id">아이디</label>
                 <br/>
@@ -37,7 +46,7 @@ const LoginForm = () => {
                 <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
                 <Link href="/signup"><a><Button>회원가입</Button></a></Link>
             </ButtonWrapper>
-        </Form>
+        </FormWrapper>
     );
 };
 
--- prepare/front/components/UserProfile.js
+++ prepare/front/components/UserProfile.js
@@ -1,5 +1,22 @@
-const UserProfile = () => {
+import React, {useCallback} from 'react';
+import {Card, Avatar, Button} from "antd";
 
+const UserProfile = ({setIsLoggedIn}) => {
+    const onLogOut = useCallback(() => {
+        setIsLoggedIn(false);
+    }, []);
+    return (
+        <Card
+            actions={[
+                <div key="twit">짹짹<br/>0</div>,
+                <div key="followings">팔로잉<br/>0</div>,
+                <div key="">팔로워<br/>0</div>,
+            ]}
+        >
+            <Card.Meta avatar={<Avatar>JY</Avatar>} title="jyryuitpro"/>
+        <Button onClick={onLogOut}>로그아웃</Button>
+        </Card>
+    );
 };
 
 export default UserProfile;
