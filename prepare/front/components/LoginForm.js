76b867486abbe1a7a7178be54a6990ce89f5c6be
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-04-29 23:42:59 +0900

    회원가입 페이지 만들기(커스텀 훅)

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -1,7 +1,10 @@
 import React, {useState, useCallback, useMemo} from 'react';
-import {Form, Input, Button} from "antd";
+import {Form, Input, Button} from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+import useInput from '../hooks/useInput'
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -12,16 +15,17 @@ const FormWrapper = styled(Form)`
 `;
 
 const LoginForm = ({setIsLoggedIn}) => {
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
+    const [id, onChangeId] = useInput('');
+    // const [id, setId] = useState('');
+    // const onChangeId = useCallback((e) => {
+    //     setId(e.target.value);
+    // }, []);
 
-    const onChangeId = useCallback((e) => {
-        setId(e.target.value);
-    }, []);
-
-    const onChangePassword = useCallback((e) => {
-        setPassword(e.target.value);
-    }, []);
+    const [password, onChangePassword] = useInput('');
+    // const [password, setPassword] = useState('');
+    // const onChangePassword = useCallback((e) => {
+    //     setPassword(e.target.value);
+    // }, []);
 
     // const style = useMemo(() => ({marginTop: 10}), []);
 
@@ -48,6 +52,10 @@ const LoginForm = ({setIsLoggedIn}) => {
             </ButtonWrapper>
         </FormWrapper>
     );
+};
+
+LoginForm.prototype = {
+    setIsLoggedIn: PropTypes.func.isRequired,
 };
 
 export default LoginForm;
